e22965a3b17e9377b36628ab19dcfc411286734b
shen-aoao <shenaoaoii@163.com>
2021-01-05 22:22:15 +0800

    商品查询

---
+++ mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/GoodsService.java
@@ -0,0 +1,22 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SpuDTO;
+import com.baidu.shop.entity.SpuEntity;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Api(tags = "商品接口")
+public interface GoodsService {
+
+    /*@ApiOperation(value = "获取spu信息")
+    @GetMapping(value = "goods/getSpuInfo")
+    public Result<List<SpuEntity>> getSpuInfo(SpuDTO spuDTO);*/
+    @ApiOperation(value = "查询spu信息")
+    @GetMapping(value = "/goods/getSpuInfo")
+    Result<PageInfo<SpuEntity>> getSpuInfo(SpuDTO spuDTO);
+}
